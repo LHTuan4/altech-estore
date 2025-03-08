48bd714658b0cfebb91953adf0c87cab60243dec
tuanlh <tuanlh@mioto.vn>
2025-03-08 20:20:23 +0700

    update response error value

--- src/main/java/com/altech/estore/response/ResponseError.java
+++ src/main/java/com/altech/estore/response/ResponseError.java
@@ -2,10 +2,10 @@ package com.altech.estore.response;
 
 public class ResponseError {
     public static final int Error_Success = 0;
-    public static final int Error_NotFound = 1;
-    public static final int Error_BadRequest = 2;
-    public static final int Error_Unauthorized = 3;
-    public static final int Error_Exception = 4;
+    public static final int Error_NotFound = -1;
+    public static final int Error_BadRequest = -2;
+    public static final int Error_Unauthorized = -3;
+    public static final int Error_Exception = -4;
 
     public static String ToErrorMessage(int error) {
         return switch (error) {
