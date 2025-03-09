b745a620bab9ea966f15f4b395d19a34db1edb31
tuanlh <tuanlh@mioto.vn>
2025-03-10 00:30:54 +0700

    update variable for params add item basket

--- src/main/java/com/altech/estore/params/ParamsAddItemBasket.java
+++ src/main/java/com/altech/estore/params/ParamsAddItemBasket.java
@@ -1,4 +1,12 @@
 package com.altech.estore.params;
 
+import com.altech.estore.entities.BasketItem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ParamsAddItemBasket {
+    private Long basketId;
+    private BasketItem request;
 }
