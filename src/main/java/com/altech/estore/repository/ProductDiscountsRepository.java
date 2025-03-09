d0fec6e3a086af39ad2118da7b6e280c3228db9f
tuanlh <tuanlh@mioto.vn>
2025-03-10 00:29:56 +0700

    update anno product repository

--- src/main/java/com/altech/estore/repository/ProductDiscountsRepository.java
+++ src/main/java/com/altech/estore/repository/ProductDiscountsRepository.java
@@ -2,6 +2,8 @@ package com.altech.estore.repository;
 
 import com.altech.estore.entities.ProductDiscountsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductDiscountsRepository extends JpaRepository<ProductDiscountsEntity, Long> {
 }
