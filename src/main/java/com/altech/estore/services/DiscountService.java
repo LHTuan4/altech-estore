9cf89752a93f498b900031937e10fa88200b2a8b
tuanlh <tuanlh@mioto.vn>
2025-03-10 00:29:24 +0700

    return empty list get discount by productId

--- src/main/java/com/altech/estore/services/DiscountService.java
+++ src/main/java/com/altech/estore/services/DiscountService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -53,6 +54,6 @@ public class DiscountService {
     public List<Long> getDiscountByProduct(Long productId) {
         return productDiscountsRepository.findById(productId)
                 .map(ProductDiscountsEntity::getDiscountIds)
-                .orElse(null);
+                .orElse(new ArrayList<>());
     }
 }
